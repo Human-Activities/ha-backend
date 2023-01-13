053149fcbe40d0169e095bcf9373a3abba604b37
MateuszKost <matekos1604@gmail.com>
2023-01-13 19:27:26 +0100

    Created group controler and service

---
+++ API/Controllers/GroupController.cs
@@ -0,0 +1,65 @@
+﻿using API.Models.Activities;
+using API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace API.Controllers
+{
+    [Route("api/group")]
+    [ApiController]
+    public class GroupController : Controller
+    {
+        private readonly GroupService _groupService;
+
+        public GroupController(GroupService groupService)
+        {
+            _groupService = groupService;
+        }
+
+        [HttpPost("create")]
+        [ProducesResponseType(typeof(CreateActivityResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> CreateGroup(CreateActivityRequest request)
+        {
+            var result = await _groupService.CreateGroup(request);
+
+            return Ok(result);
+        }
+
+        [HttpGet("get/{groupId:int}")]
+        [ProducesResponseType(typeof(GetActivityResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetGroup(int groupId)
+        {
+            var result = await _groupService.GetGroup(groupId);
+
+            return Ok(result);
+        }
+
+        [HttpGet("get")]
+        [ProducesResponseType(typeof(GetActivitiesResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> GetGroups(GetActivitiesRequest request)
+        {
+            string userId = HttpContext.User.FindFirstValue("id");
+            var result = await _groupService.GetGroups(request, userId);
+
+            return Ok(result);
+        }
+
+        [HttpPut("edit")]
+        [ProducesResponseType(typeof(EditActivityResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> EditGroup(EditActivityRequest request)
+        {
+            var result = await _groupService.EditGroup(request);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("delete/{groupId:int}")]
+        [ProducesResponseType(typeof(DeleteActivityResult), StatusCodes.Status200OK)]
+        public async Task<IActionResult> DeleteGroup(int groupId)
+        {
+            var result = await _groupService.DeleteGroup(groupId);
+
+            return Ok(result);
+        }
+    }
+}
---
+++ API/Services/GroupService.cs
@@ -0,0 +1,107 @@
+﻿using API.Exceptions;
+using API.Models.Activities;
+using DAL.UnitOfWork;
+using DAL;
+
+namespace API.Services
+{
+    public class GroupService
+    {
+        private readonly IUnitOfWork _uow;
+
+        public GroupService()
+        {
+            _uow = DataAccessLayerFactory.CreateUnitOfWork();
+        }
+
+        public async Task<CreateActivityResult> CreateGroup(CreateActivityRequest request)
+        {
+            if (request.Name.IsNullOrEmpty())
+                throw new OperationException(StatusCodes.Status400BadRequest, "Activity name can't be empty");
+
+            var activity = new Group
+            {
+                Name = request.Name,
+                Description = request.Description,
+                IsPrivate = request.IsPrivate
+            };
+
+            await _uow.ActivityRepo.AddAsync(activity);
+            await _uow.CompleteAsync();
+
+            return new CreateActivityResult("Activity has been created succesfully!");
+        }
+
+        public async Task<GetActivityResult> GetGroup(int groupId)
+        {
+            var activity = await _uow.ActivityRepo.FindAsync(groupId);
+
+            return new GetActivityResult
+            {
+                Name = activity.Name,
+                Description = activity.Description,
+                IsPrivate = activity.IsPrivate,
+                IsTemplate = activity.IsTemplate,
+                Categories = activity.Categories
+            };
+        }
+
+        public async Task<IEnumerable<GetActivitiesResult>> GetGroups(GetActivitiesRequest request, string userId)
+        {
+            var activities = await _uow.ActivityRepo.WhereAsync(a => a.IsPrivate == request.IsPrivate && a.User.Id == Guid.Parse(userId));
+
+            return activities.Select(a => a.ToGetActivitiesResult());
+        }
+
+        public async Task<EditActivityResult> EditGroup(EditActivityRequest request)
+        {
+            if (request.Name.IsNullOrEmpty())
+                throw new OperationException(StatusCodes.Status400BadRequest, "Activity name can't be empty");
+
+            var activity = await _uow.ActivityRepo.FindAsync(request.Id);
+
+            if (activity == null)
+                throw new OperationException(StatusCodes.Status500InternalServerError, "Internal server error. There is no activity like this");
+
+            activity.Name = request.Name;
+            activity.Description = request.Description;
+            activity.IsPrivate = request.IsPrivate;
+            activity.IsTemplate = request.IsTemplate;
+
+            _uow.ActivityRepo.Update(activity);
+            await _uow.CompleteAsync();
+
+            return new EditActivityResult("Activity has been edited successfully!");
+        }
+
+        public async Task<DeleteActivityResult> DeleteGroup(int groupId)
+        {
+            var activity = await _uow.ActivityRepo.FindAsync(groupId);
+
+            if (activity == null)
+                throw new OperationException(StatusCodes.Status500InternalServerError, "Internal server error. There is no activity like this");
+
+            _uow.ActivityRepo.Remove(activity);
+            await _uow.CompleteAsync();
+
+            return new DeleteActivityResult("Activity has been deleted successfully!");
+        }
+    }
+}
+
+public static class ActivityServiceExtensions
+{
+    public static GetActivitiesResult ToGetActivitiesResult(this Activity activity)
+    {
+        return new GetActivitiesResult
+        {
+            Id = activity.Id,
+            Name = activity.Name,
+            Description = activity.Description,
+            IsPrivate = activity.IsPrivate,
+            IsTemplate = activity.IsTemplate,
+            Categories = activity.Categories
+        };
+    }
+}
+
