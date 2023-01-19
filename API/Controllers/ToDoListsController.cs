888a4968b7adfd2a3063b75904bfce3e2a527e8a
MateuszKost <matekos1604@gmail.com>
2023-01-19 21:17:02 +0100

    created toDoList controller and service

---
+++ API/Controllers/ToDoListsController.cs
@@ -0,0 +1,17 @@
+﻿using API.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/TodoLists")]
+    [ApiController]
+    public class ToDoListsController : Controller
+    {
+        private readonly ToDoListsService _toDoListService;
+
+        public ToDoListsController(ToDoListsService toDoListService)
+        {
+            _toDoListService = toDoListService;
+        }
+    }
+}
---
+++ API/Services/ToDoListsService.cs
@@ -0,0 +1,15 @@
+﻿using DAL;
+using DAL.UnitOfWork;
+
+namespace API.Services
+{
+    public class ToDoListsService
+    {
+        private readonly IUnitOfWork _uow;
+
+        public ToDoListsService()
+        {
+            _uow = DataAccessLayerFactory.CreateUnitOfWork();
+        }
+    }
+}
