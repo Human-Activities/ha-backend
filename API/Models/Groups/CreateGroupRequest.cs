5b92d425dcee80f8a551d23025647cd907e184ce
MateuszKost <matekos1604@gmail.com>
2023-02-16 23:56:47 +0100

    Update group request

--- API/Models/Groups/CreateGroupRequest.cs
+++ API/Models/Groups/CreateGroupRequest.cs
@@ -6,6 +6,6 @@ namespace API.Models.Groups
     {
         public string Name { get; set; }
 
-        public string Description { get; set; }
+        public string? Description { get; set; }
     }
 }
